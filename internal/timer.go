517a7e0668499cf968b703b52a671013ca44040f
shale <slucas001@citymail.cuny.edu>
2024-03-03 22:33:12 -0500

    started updateTui

--- internal/timer.go
+++ internal/timer.go
@@ -6,17 +6,17 @@ import (
 )
 
 // Decrement the time object passed 	
-func updateTime (t *time.Time) {	
-	if !t.IsZero() {
-		*t = t.Add(-1 * time.Second); 	
+func updateTime (timeLeft *time.Time) time.Time {	
+	if !timeLeft.IsZero() {
+		*timeLeft = timeLeft.Add(-1 * time.Second); 	
 	} else {
-		// Do something else
+		// Return the 'Zero Time'
+		return timeLeft	
 	} 
 
 }
 
-// TODO: Implement this function... 
-func updateScreen() {
-	
-	screenTime := updateTime()
+func updateTui(timeLeft) string {	
+	tuiTime := updateTime()
+	tuiTime.String()
 }	
