557f1c36db448d14ba392d6dabadb01a260c5cac
shale <slucas001@citymail.cuny.edu>
2024-03-03 23:32:23 -0500

    refactored functions for time

--- internal/timer.go
+++ internal/timer.go
@@ -6,17 +6,13 @@ import (
 )
 
 // Decrement the time object passed 	
-func updateTime (timeLeft *time.Time) time.Time {	
-	if !timeLeft.IsZero() {
-		*timeLeft = timeLeft.Add(-1 * time.Second); 	
-	} else {
-		// Return the 'Zero Time'
-		return timeLeft	
-	} 
-
+func UpdateTime (timeLeft *time.Time) time.Time {	
+	// Return the time regardless
+	return timeLeft.Add(-1 * time.Second)
 }
 
-func updateTui(timeLeft) string {	
-	tuiTime := updateTime()
-	tuiTime.String()
-}	
+func UpdateTui(timeLeft *time.Time) string {	
+	// Format the time into a string	
+	return tuiTime.Format("00:00:00")
+}
+
