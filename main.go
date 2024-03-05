af152b4c4978aaf314f1a0614b1f62200362ea66
shale <slucas001@citymail.cuny.edu>
2024-03-04 23:14:11 -0500

    centered display box for showing time

--- main.go
+++ main.go
@@ -30,23 +30,25 @@ func main() {
 
 func layout(g *gocui.Gui) error {
     maxX, maxY := g.Size()
-    if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+    if v, err := g.SetView("hello", (maxX/2) - 15 , (maxY/2) - 5 , (maxX/2) + 15,  (maxY/2) + 5); err != nil {
         if err != gocui.ErrUnknownView {
             return err
         }
-        v.BgColor = gocui.ColorBlue // Changing background color
+
+		timerAmount, _ := internal.ParseInputs()
+		timeStr := timerAmount.Format("00:00:00")
+        fmt.Fprintln(v, timeStr) 
+
+        v.BgColor = gocui.ColorBlack // Changing background color
         v.FgColor = gocui.ColorWhite // Changing font color
-        v.SelBgColor = gocui.ColorGreen // Changing selected background color
-        v.SelFgColor = gocui.ColorBlack // Changing selected font color
+        v.SelBgColor = gocui.ColorDefault // Changing selected background color
+        v.SelFgColor = gocui.ColorWhite // Changing selected font color
         v.Wrap = true // Enabling text wrapping
         v.Frame = true // Enable a frame around the view
-        v.Title = "Hello View" // Setting a title for the view
+        v.Title = "timer"// Setting a title for the view
         v.Autoscroll = true // Automatically scroll to the bottom when new content is added
         v.Highlight = true // Enable highlighting
 		
-		// Parse CLI
-		timerAmount, _ := internal.ParseInputs()
-        fmt.Fprintln(v, timerAmount.Format())
     }
     return nil
 }
