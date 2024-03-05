3ba1304ca8e5138a60d0a28de63ee6aeeec1eead
shale <slucas001@citymail.cuny.edu>
2024-03-04 23:24:50 -0500

    added possible new func for alerting on half and 3/4 done timers

--- internal/timer.go
+++ internal/timer.go
@@ -10,6 +10,18 @@ func UpdateTime (timeLeft *time.Time) time.Time {
 	return timeLeft.Add(-1 * time.Second)
 }
 
+// 'alert' halfway
+func  ... (timeLeft time.Time) bool {
+	
+}
+
+
+// 'alert' 3/4 of the way
+func  ... (timeLeft time.Time) bool {
+	
+}
+
+
 func UpdateTui(timeLeft *time.Time) string {	
 	// Format the time into a string	
 	return timeLeft.Format("00:00:00")
