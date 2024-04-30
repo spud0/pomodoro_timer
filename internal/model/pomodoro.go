302903c6bd4cf0d0a358cd7cc96cd48d208ea2ab
shale <slucas001@citymail.cuny.edu>
2024-04-29 23:28:01 -0400

    added the init func, and start

--- internal/model/pomodoro.go
+++ internal/model/pomodoro.go
@@ -1,6 +1,31 @@
+package model
 
-// Structure definition
+import (
+	"time"
+)
+
 type Pomodoro struct {
+	Active		bool 			// State for if the timer is still active. 
+	StartTime 	time.Time
+	EndTime 	time.Time	
+	Duration	time.Duration
+	StopAfter 	time.Duration	// Stop the timer for a break after X (time units). 
+}
 
+// Creates a new Pomodoro object 
+func NewPom (dur, stopAfter time.Duratino) *Pomodoro {
+	
+	return &Pomodoro {
+		Active : false, 
+		Duration: dur, 
+		StopAfter : stopAfter, 
+	} 
+}
 
+// Starts the timer
+func (pom *Pomodoro) Start() {
+
+	pom.Active = true
+	pom.StartTime = time.Now()
+	pom.EndTime = pom.StartTime.Add(pom.Duration)
 }
