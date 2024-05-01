26abff6b49e874d3ddd4e3075a4792ba4f4d8b99
shale <slucas001@citymail.cuny.edu>
2024-04-30 23:40:32 -0400

    fixed formatting, added remaining time func, and fixed typo

--- internal/model/pomodoro.go
+++ internal/model/pomodoro.go
@@ -13,7 +13,7 @@ type Pomodoro struct {
 }
 
 // Creates a new Pomodoro object 
-func NewPom (dur, stopAfter time.Duratino) *Pomodoro {
+func NewPom (dur, stopAfter time.Duration) *Pomodoro {
 	
 	return &Pomodoro {
 		Active : false, 
@@ -27,15 +27,24 @@ func (pom *Pomodoro) Start() {
 	pom.Active = true
 	pom.StartTime = time.Now(
 	
-	)// Add the duration to the start to get the EndTime
+	) // Add the duration to the start to get the EndTime
 	pom.EndTime = pom.StartTime.Add(pom.Duration) 
 }
 
-
+// Stops the timer
 func (pom *Pomodoro) Stop() {
 	pom.Active = false
 }
 
+
+// Determines the remaining time
+func (pom *Pomodoro) RemainingTime() time.Duration {
+	if pom.Active {
+		return pom.EndTime.Sub(time.Now())
+	}
+
+	return 0 
+}
 
 
 
