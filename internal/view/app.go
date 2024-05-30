b74b92dd7e11afa8fd425bf329b73e2ec73c5c84
shale <slucas001@citymail.cuny.edu>
2024-05-30 02:23:45 -0400

    fixed import, impl'd Update and View

--- internal/view/app.go
+++ internal/view/app.go
@@ -2,7 +2,11 @@ package view
 
 import (
 	"fmt"
+	"strings"
 	"time"
+	
+	"github.com/spud0/pomodoro_timer/internal/model"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 // Access the model package, for the Pomodoro Object
@@ -33,13 +37,47 @@ func (a app) Init () tea.Cmd {
 
 // The Update function, 
 func (a app) Update (msg tea.Msg) (tea.Model, tea.Cmd) {
-	
-	switch msg.(type) {
-		case tea.KeyMsg: 
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:	
+		switch msg.String() {
+			case "ctrl+c", "q": 
+				return a, tea.Quit
 			
-		case tea.KeyMsg: 		
-			return a, nil
-	} 
+			// case : return a, nil
+		} 
+
+	case tea.TickMsg:
+		if a.Pomodoro.Active  {
+			return a, tea.Tick(time.Second, 
+				func(time.Time) tea.Msg {
+					return tea.TickMsg{}	
+			})
+		} 
+
+	}
+}
 
 	return a, nil
+}
+
+func (a app) View () string {
+	
+	var result strings.Builder
+
+	if a.Pomodoro.Active {
+		remTime = a.Pomodoro.RemainingTime()
+		mins = int(rem.Minutes()) 
+		secs = int(rem.Seconds()) % 60
+
+		fmt.Fprintf(&result, "pom timer: %02d:%02d\n", mins, secs)
+
+		result.WriteString(" [p] : pause, [q] / [esc]: quit")
+	} else {
+
+		result.WriteString("pom isn't active...")
+		result.WriteString(" [p] : pause, [q] / [esc] : quit")
+	}
+
+	return result.String()
 }
