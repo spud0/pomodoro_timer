b07a6e43a4f8e6dbfb70db9dc5fb8df9ae0380de
shale <slucas001@citymail.cuny.edu>
2024-07-02 23:55:57 -0400

    fixed main

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"flag"
-	"time"
+    "time"
 
-	"github.com/spud0/pomodoro_timer/internal"
-	tea "github.com/charmbracelet/bubbletea/"
+    "github.com/spud0/pomodoro_timer/internal/model"
+    "github.com/spud0/pomodoro_timer/internal/view"
+    tea "github.com/charmbracelet/bubbletea"
 )
 
-// Contains the entry point
-func main () {
-	
-	durFlag := flag.Duration("dur", 2 * time.Minute, "duration of the timer")
-	breakFlag  := flag.Duration("break", 1 * time.Minute, "break time during timer")
-
-	flag.Parse()
-	
-	pom := model.NewPomodoro(*durFlag, *breakFlag)
-	session := tea.NewProgram(view.NewApp(pom))
-
-	if err := session.Start(); err != nil {
-		panic(err)
-	} 
+func main() {
+    pom := model.NewPom(25*time.Minute, 5*time.Minute)
+    app := view.NewApp(pom)
+    p := tea.NewProgram(app)
+    if err := p.Start(); err != nil {
+        panic(err)
+    }
 }
+
